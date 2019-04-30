3ac78a5e3910bfbdabfcced9377625f31ccd484e
rutgerengeldev <engelrutger94@gmail.com>
2019-04-30 11:37:57 +0200

    Updates

---
+++ precache-manifest.beb376282b18a3556204f09bb743c37c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "22b2d25f3a4cbd75f607",
+    "url": "/robofriends/static/js/main.01954cd1.chunk.js"
+  },
+  {
+    "revision": "615c8eba5bd18a798459",
+    "url": "/robofriends/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "22b2d25f3a4cbd75f607",
+    "url": "/robofriends/static/css/main.7599486d.chunk.css"
+  },
+  {
+    "revision": "615c8eba5bd18a798459",
+    "url": "/robofriends/static/css/2.d1002d30.chunk.css"
+  },
+  {
+    "revision": "a986e578d06b2cc9f6833d7ac80e2851",
+    "url": "/robofriends/index.html"
+  }
+];
